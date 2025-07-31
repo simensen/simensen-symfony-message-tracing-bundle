790b4a3e090f4b85207f526cd7cc3cc1ef299e1f
David Flanagan <david@rawkode.dev>
2025-07-31 23:10:05 +0100

    feat: build parle as nix package

--- flake.nix
+++ flake.nix
@@ -6,13 +6,45 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        
+
+        parle = pkgs.php.buildPecl {
+          pname = "parle";
+          version = "0.8.5";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "weltling";
+            repo = "parle";
+            rev = "parle-0.8.5";
+            sha256 = "sha256-Z7MEOESu4qhefIX3y5YjomZXmZutVqKXHTbRRwn37ZQ=";
+          };
+
+          meta = with pkgs.lib; {
+            description = "Parsing and lexing";
+            homepage = "https://github.com/weltling/parle";
+            license = licenses.bsd2;
+          };
+        };
+
         configuredPkgs = {
-          php = pkgs.php.withExtensions ({ all, enabled }: enabled ++ (with all; [ gnupg xdebug ]));
+          php = pkgs.php.withExtensions (
+            { all, enabled }:
+            enabled
+            ++ (with all; [
+              gnupg
+              xdebug
+            ])
+            ++ [ parle ]
+          );
         };
       in
       {
@@ -30,5 +62,6 @@
             export PHP_CS_FIXER_IGNORE_ENV=1
           '';
         };
-      });
+      }
+    );
 }
